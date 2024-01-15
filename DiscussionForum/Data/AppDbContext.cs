b9b21f3b57cc6aec0206d00dd64ebdd160adb26a
Sreelakshmi P M <sree2000lachu@gmail.com>
2024-01-15 19:42:24 +0530

    feat:- Added repository pattern

--- DiscussionForum/Data/AppDbContext.cs
+++ DiscussionForum/Data/AppDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace DiscussionForum.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiscussionForum.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
--- DiscussionForum/UnitOfWork/UnitOfWork.cs
+++ DiscussionForum/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,28 @@
-﻿namespace DiscussionForum.UnitOfWork
+﻿using DiscussionForum.Data;
+
+namespace DiscussionForum.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            // Initialize other repositories.
+        }
+
+
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
     }
 }
