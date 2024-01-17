2bd4adde4b932befe1ed660858c4a13661e0d539
Sreelakshmi P M <sree2000lachu@gmail.com>
2024-01-17 17:15:37 +0530

    feat:-Added forumCategory table APIs - GET,GETByID,POST,PUT,DELETE

--- DiscussionForum/Models/EntityModels/ForumCategory.cs
+++ DiscussionForum/Models/EntityModels/ForumCategory.cs
@@ -1,6 +1,23 @@
-﻿namespace DiscussionForum.Models.EntityModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiscussionForum.Models.EntityModels
 {
     public class ForumCategory
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ForumCategoryID { get; set; }
+
+        [Required(ErrorMessage = "Forum category name is required.")]
+        [StringLength(100, ErrorMessage = "Forum category name cannot exceed 100 characters.")]
+        public string ForumCategoryName { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ForumCategory()
+        {
+            // Set default values in the constructor
+            IsDeleted = false;
+        }
     }
 }
