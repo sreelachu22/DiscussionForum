cf1ba6926441a69b1b4368732fda4dee43074774
Sreelakshmi P M <sree2000lachu@gmail.com>
2024-01-18 09:49:17 +0530

    fix:- fixed conflicts after merging

--- DiscussionForum/UnitOfWork/UnitOfWork.cs
+++ DiscussionForum/UnitOfWork/UnitOfWork.cs
@@ -1,34 +1,42 @@
 ﻿using DiscussionForum.Data;
 using DiscussionForum.Repositories;
 using DiscussionForum.Models.EntityModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiscussionForum.UnitOfWork
 {
     //namespace ThreadStatusService.Infrastructure.UnitOfWork
     //{
-        public class UnitOfWork : IUnitOfWork
+
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
         {
-          _context = context?? throw new ArgumentNullException(nameof(context));
-            
+            _context = context;
+
             Role = new RoleRepository(_context);
             ForumCategory = new ForumCategoryRepository(_context);
             ForumStatus = new ForumStatusRepository(_context);
             ThreadStatus = new ThreadStatusRepository(_context);
             // Initialize other repositories.
         }
-        
+
         public IRoleRepository Role { get; }
         public IForumStatusRepository ForumStatus { get; }
-        public IForumCategoryRepository ForumCategory { get;}
+        public IForumCategoryRepository ForumCategory { get; }
         public IThreadStatusRepository ThreadStatus { get; }
-  
+
         public int Complete()
         {
             return _context.SaveChanges();
         }
-  
-        int IUnitOfWork.Complete()
+
+        /*int IUnitOfWork.Complete()
         {
             throw new NotImplementedException();
-        }
+        }*/
+    }
 }
