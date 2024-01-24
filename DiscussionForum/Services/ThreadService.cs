21b367c20aa995ecd5b4969b733deb4ca7d6d975
Libin Prince <libin.prince@experionglobal.com>
2024-01-24 17:54:15 +0530

    feat/Df012 fixes with pagination

--- DiscussionForum/Services/ThreadService.cs
+++ DiscussionForum/Services/ThreadService.cs
@@ -54,7 +54,7 @@ namespace DiscussionForum.Services
             }
             catch (Exception ex)
             {
-                throw new Exception("Error  while fetching threads", ex);
+                throw new Exception("Error  while fetching threads.", ex);
             }
         }
     }
