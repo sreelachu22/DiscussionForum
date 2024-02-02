88c08cbdb881876b315992bb89564342b74049f3
Abraham Jacob Kurialanickal <abraham.kurialanickal@experionglobal.com>
2024-02-02 15:21:30 +0530

    feat:- added GETbyID for community

--- DiscussionForum/Controllers/CommunityController.cs
+++ DiscussionForum/Controllers/CommunityController.cs
@@ -39,6 +39,44 @@ namespace DiscussionForum.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves a specific community.
+        /// </summary>
+        /// <param name="communityId">The ID of the community to be retrieved</param>
+        [HttpGet("{communityId}")]
+        public async Task<IActionResult> GetCommunityById(int communityId)
+        {
+            try
+            {
+                //Validates the request data
+                if (communityId <= 0)
+                {
+                    throw new Exception("Invalid communityId. It should be greater than zero.");
+                }
+
+                var _community = await _communityService.GetCommunityByIdAsync(communityId);
+
+                //Checks if the retrieved community is null
+                if (_community == null)
+                {
+                    throw new Exception($"Community with ID {communityId} not found.");
+                }
+
+                return Ok(_community);
+
+            }
+            catch (Exception ex)
+            {
+                //Checks for an inner exception and returns corresponding error message
+                if (ex.InnerException != null)
+                {
+                    return StatusCode(500, $"Error while retrieving community with ID = {communityId} \nError: {ex.InnerException.Message}");
+                }
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, $"Error while retrieving community with ID = {communityId} \nError: {ex.Message}");
+            }
+        }
+
     }
 
 }
